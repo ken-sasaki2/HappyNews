08309324454d37f10e51ae23438c759ea2206a89
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-08-13 11:51:59 +0900

    TopNews update
    必要なライブラリの呼び出しとプロトコルの宣言。

--- HappyNews/Controller/TopNewsTableViewController.swift
+++ HappyNews/Controller/TopNewsTableViewController.swift
@@ -7,32 +7,30 @@
 //
 
 import UIKit
+import SegementSlide
 
-class TopNewsTableViewController: UITableViewController {
+class TopNewsTableViewController: UITableViewController,SegementSlideContentScrollViewDelegate, XMLParserDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
     // MARK: - Table view data source
 
+    //セルのセクションを決めるメソッド
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
-
+    
+    //セルの数を決めるメソッド
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        
         return 0
     }
-
-    /*
+    
+    //セルを構築する際に呼ばれるメソッド
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
@@ -40,7 +38,6 @@ class TopNewsTableViewController: UITableViewController {
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
