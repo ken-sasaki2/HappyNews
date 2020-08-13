c2a954b08dceb9e05f9a4d34a1f05664b5e61cb9
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-08-13 09:39:21 +0900

    Initial commit
    プロジェクトの作成

---
+++ HappyNews/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyNews
+//
+//  Created by 佐々木　謙 on 2020/08/13.
+//  Copyright © 2020 佐々木　謙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
