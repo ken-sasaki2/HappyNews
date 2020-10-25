470ce303b0847554eabf6b55c724951447997e72
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-10-25 17:03:37 +0900

    update LanguageTranslator
    ToneAnalyzerModelの作成。また、初期値を設定し、通信を行える環境を整える。

--- HappyNews/Model/ToneAnalyzerModel.swift
+++ HappyNews/Model/ToneAnalyzerModel.swift
@@ -4,6 +4,109 @@
 //
 //  Created by 佐々木　謙 on 2020/10/23.
 //  Copyright © 2020 佐々木　謙. All rights reserved.
-//
+
 
 import Foundation
+import ToneAnalyzer
+import SwiftyJSON
+
+class ToneAnalyzerModel {
+    
+    //外部から渡ってくる値
+    var analysisKey  : String?
+    var version      : String?
+    var serviceURL   : String?
+    var analysisText : String?
+    
+    //JSON解析で使用
+    var count = 0
+    
+    //NewsTableViewから値を受け取る
+    init(analysisApiKey: String, analysisVersion: String, analysisURL: String, sampleText: String) {
+        
+        analysisKey   = analysisApiKey
+        version       = analysisVersion
+        serviceURL    = analysisURL
+        analysisText  = sampleText
+    }
+    
+    //感情分析
+    func setToneAnalyzer() {
+        
+        //APIを認証するためのキーとversionとurlを定義
+        let toneAnalyzerKey         = WatsonIAMAuthenticator(apiKey: analysisKey!)
+        let toneAnalyzer            = ToneAnalyzer(version: version!, authenticator: toneAnalyzerKey)
+            toneAnalyzer.serviceURL = serviceURL
+
+        //sampleTextを分析（リクエスト送信）
+        toneAnalyzer.tone(toneContent: .text(analysisText!)) {
+          response, error in
+
+          //エラー処理
+          if let error = error {
+            switch error {
+            case let .http(statusCode, message, metadata):
+                switch statusCode {
+                case .some(404):
+                    // Handle Not Found (404) exceptz1zion
+                    print("Not found")
+                case .some(413):
+                    // Handle Request Too Large (413) exception
+                    print("Payload too large")
+            default:
+                if let statusCode = statusCode {
+                    print("Error - code: \(statusCode), \(message ?? "")")
+                }
+            }
+            default:
+              print(error.localizedDescription)
+            }
+            return
+          }
+
+          //データ処理
+          guard let toneAnalysisResult = response?.result else {
+            print(error?.localizedDescription ?? "unknown error")
+            return
+          }
+
+          //レスポンスの結果で条件分岐
+          let statusCode = response?.statusCode
+            switch statusCode == Optional(200) {
+            case true:
+                //分析結果のデータをJSON形式に変換
+                let encoder = JSONEncoder()
+                encoder.outputFormatting = .prettyPrinted
+                guard let toneAnalysisJSON = try? encoder.encode(toneAnalysisResult) else {
+                    fatalError("Failed to encode to JSON.")
+                }
+
+                //JSONデータ確認
+                print("toneAnalysisJSON: \(String(bytes: toneAnalysisJSON, encoding: .utf8)!)")
+
+                //JSON解析(score)&小数点を切り上げて取得
+                let toneAnalysisValue = JSON(toneAnalysisJSON)
+                let firstToneScore    = toneAnalysisValue["document_tone"]["tones"][self.count]["score"].float
+                let firstScore        = ceil(firstToneScore! * 100)/100
+
+                let secondToneScore   = toneAnalysisValue["document_tone"]["tones"][self.count+1]["score"].float
+                let secondScore       = ceil(secondToneScore! * 100)/100
+
+                //JSON解析(tone_name)
+                let firstToneName  = toneAnalysisValue["document_tone"]["tones"][self.count]["tone_name"].string
+                let secondToneName = toneAnalysisValue["document_tone"]["tones"][self.count+1]["tone_name"].string
+
+                //感情分析結果確認
+                print("*****感情分析結果確認*****")
+                print("score     : \(firstScore)")
+                print("score     : \(secondScore)")
+                print("tone_name : \(firstToneName)")
+                print("tone_name : \(secondToneName)")
+
+            case false:
+                //ステータスコードの表示(200範囲は成功、400範囲は障害、500範囲は内部システムエラー)
+                print("analysis failure: \(statusCode)")
+            }
+        }
+    }
+}
