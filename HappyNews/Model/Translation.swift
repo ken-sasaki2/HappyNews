91f8f56e085f8ff52d4a4baf9b67ca2c750c50a0
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-10-25 19:28:25 +0900

    update LanguageTranslation
    構造体を作成し、翻訳結果を構造体へ追加

---
+++ HappyNews/Model/Translation.swift
@@ -0,0 +1,19 @@
+//
+//  TranslationResult.swift
+//  HappyNews
+//
+//  Created by 佐々木　謙 on 2020/10/25.
+//  Copyright © 2020 佐々木　謙. All rights reserved.
+//
+
+import Foundation
+
+struct Translation {
+    
+    var translation: String?
+    
+    //構造体の初期化
+    init(translation: String) {
+        self.translation = translation
+    }
+}
