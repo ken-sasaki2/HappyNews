41c5b315747699d6b779e9b4d335b50a4136c9f4
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-11-01 17:46:45 +0900

    update master
    感情分析のJSON解析の条件を編集

--- HappyNews/Model/ToneAnalyzerModel.swift
+++ HappyNews/Model/ToneAnalyzerModel.swift
@@ -102,7 +102,7 @@ class ToneAnalyzerModel {
                 print("")
               
                 //感情分析結果が"Joy"&"Score"が0.7以上なら値を取得
-                if toneAnalysisValue["document_tone"]["tones"][self.count]["tone_name"] == "Joy" && toneAnalysisValue["document_tone"]["tones"][self.count]["score"] > 0.7 {
+                if toneAnalysisValue["document_tone"]["tones"][self.count]["tone_name"] == "Joy" && toneAnalysisValue["document_tone"]["tones"][self.count]["score"] > 0.6 {
                     
                     let  firstToneScore   = toneAnalysisValue["document_tone"]["tones"][self.count]["score"].float
                     self.firstScore       = ceil(firstToneScore! * 100)/100
@@ -113,6 +113,7 @@ class ToneAnalyzerModel {
                     print("firstScore   : \(self.firstScore)")
                     print("firstToneName: \(self.firstToneName)")
                 } else {
+                    
                     print("Not a joy and a low score, so we didn't get it.")
                 }
     
