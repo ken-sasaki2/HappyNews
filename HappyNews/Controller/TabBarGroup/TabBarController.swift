696e010f66bd134efbb2605987b4dfe78eac9b89
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-11-19 05:58:52 +0900

    update master
    起動時の色の設定と、タブ選択ごとにitemの殻を変更。NavigationBarと配色を合わせるため。

--- HappyNews/Controller/TabBarGroup/TabBarController.swift
+++ HappyNews/Controller/TabBarGroup/TabBarController.swift
@@ -8,46 +8,33 @@
 
 import UIKit
 
-//16進数color 機能拡張
-extension UIColor {
-    convenience init(hex: String, alpha: CGFloat = 1.0) {
-        let v = Int("000000" + hex, radix: 16) ?? 0
-        let r = CGFloat(v / Int(powf(256, 2)) % 256) / 255
-        let g = CGFloat(v / Int(powf(256, 1)) % 256) / 255
-        let b = CGFloat(v / Int(powf(256, 0)) % 256) / 255
-        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
-    }
-}
-
 class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
+        //起動時のitemの色
+        UITabBar.appearance().tintColor = UIColor(hex: "2DCCD3")
+        
         //tabbarの色を変更
         tabBar.barTintColor = UIColor(hex: "ffffff")
     
         //tabbar背景の透過
         UITabBar.appearance().backgroundImage = UIImage()
-    
-        //tabbarの選択色の設定
-        tabBar.tintColor = UIColor(hex: "2DCCD3")
-    
-        //tabbarの非選択色の設定
-        tabBar.unselectedItemTintColor =  UIColor.gray
     }
     
     //tabbarをタップした場合のアクション
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
         switch item.title {
         case "ニュース":
-            print("タップするとTOPにスクロールして欲しい")
+            tabBar.tintColor = UIColor(hex: "2DCCD3")
         case "天気":
-            print("アクション未定")
+            tabBar.tintColor = UIColor(hex: "ff4500")
         case "アカウント":
-            print("アクション未定")
+            tabBar.tintColor = UIColor(hex: "ffa500")
         default:
-            break
+            //tabbarの非選択色の設定
+            tabBar.unselectedItemTintColor =  UIColor.gray
         }
     }
 }
