050788b8523607f1de54cab0196f2b08d6e2db62
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-12-09 16:44:07 +0900

    update edit-tableView2
    感情分析処理を直列処理で実行。
    感情分析前と後で配列の順番を合わせる必要があるため。

--- HappyNews/Model/ToneAnalyzerModel.swift
+++ HappyNews/Model/ToneAnalyzerModel.swift
@@ -9,8 +9,8 @@ import Foundation
 import ToneAnalyzer
 import SwiftyJSON
 
+//Controllerに値を渡す
 protocol DoneCatchAnalyzerProtocol {
-    
     func catchAnalyzer(arrayAnalyzerData: Array<Any>)
 }
 
@@ -53,75 +53,85 @@ class ToneAnalyzerModel {
         let toneAnalyzer    = ToneAnalyzer(version: toneAnalyzerAccessVersion!, authenticator: toneAnalyzerKey)
         toneAnalyzer.serviceURL = toneAnalyzerAccessURL
         
+        //直列処理で使用するインスタンス
+        let dispatchGroup = DispatchGroup()
+        let dispatchQueue = DispatchQueue(label: "queue")
+        
         for i in 0..<50 {
-            //リクエスト送信
-            toneAnalyzer.tone(toneContent: .text(toneAnalyzerArray[i])) {
-                response, error in
+            
+            //直列処理開始
+            dispatchGroup.enter()
+            dispatchQueue.async(group: dispatchGroup) {
+                print("#\(i)Start")
                 
-                //エラー処理
-                if let error = error {
-                    switch error {
-                    case let .http(statusCode, message, metadata):
-                        switch statusCode {
-                        case .some(404):
-                            print("Handle Not Found (404) exceptz1zion")
-                        case .some(413):
-                            print("Handle Request Too Large (413) exception")
-                        case .some(429):
-                            //429エラーが発生すると意図する値を作成してappend
-                            self.errorResponse = "errorResponse 429 error occurred"
-                            print(self.errorResponse)
-                            self.errorResult = JSON(self.errorResponse)
-                            self.toneAnalysisArray.append(self.errorResult)
-                        default:
-                            if let statusCode = statusCode {
-                                print("Error - code: \(statusCode), \(message ?? "")")
+                //リクエスト送信
+                toneAnalyzer.tone(toneContent: .text(self.toneAnalyzerArray[i])) {
+                    response, error in
+                    
+                    //エラー処理
+                    if let error = error {
+                        switch error {
+                        case let .http(statusCode, message, metadata):
+                            switch statusCode {
+                            case .some(404):
+                                print("Handle Not Found (404) exceptz1zion")
+                            case .some(413):
+                                print("Handle Request Too Large (413) exception")
+                            case .some(429):
+                                //429エラーが発生すると意図する値を作成してappend
+                                self.errorResponse = "errorResponse 429 error occurred"
+                                print(self.errorResponse)
+                                self.errorResult = JSON(self.errorResponse)
+                                self.toneAnalysisArray.append(self.errorResult)
+                            default:
+                                if let statusCode = statusCode {
+                                    print("Error - code: \(statusCode), \(message ?? "")")
+                                }
                             }
+                        default:
+                            print(error.localizedDescription)
                         }
-                    default:
-                        print(error.localizedDescription)
-                    }
-                    return
-                }
-                
-                //toneAnalyzerResult = レスポンス結果
-                guard let toneAnalyzerResult = response?.result else {
-                    print(error?.localizedDescription ?? "unknown error")
-                    return
-                }
-                
-                print("count: \(i)")
-               //print("toneAnalyzerResult: \(toneAnalyzerResult)")
-                
-                //レスポンスのステータスコードで条件分岐
-                let statusCode = response?.statusCode
-                switch statusCode == Optional(200) {
-                case true:
-                    //取得対象をdocument_toneに設定し、JSON形式に整形
-                    let encoder = JSONEncoder()
-                    encoder.outputFormatting = .prettyPrinted
-                    guard let toneAnalysisJSON = try? encoder.encode(toneAnalyzerResult.documentTone) else {
-                        fatalError("Failed to encode to JSON.")
+                        return
                     }
                     
-                    //正常に分析されたテキストをSwiftyJSONのJSON型へ変換
-                    let toneAnalysisValue = JSON(toneAnalysisJSON)
-//                    print(toneAnalysisValue.description)
-                    
-                    //ここが一つの処理の終着点（通信が終了したということと定義）
-                    //感情分析結果を配列に保存
-                    self.toneAnalysisArray.append(toneAnalysisValue)
-
-                    //感情分析結果がXMLの要素数と一致していれば実行
-                    if self.toneAnalysisArray.count == self.arrayCount {
-                        //jsonAnalysisOfToneAnalyzerの呼び出し
-//                        self.jsonAnalysisOfToneAnalyzer()
+                    //toneAnalyzerResult = レスポンス結果
+                    guard let toneAnalyzerResult = response?.result else {
+                        print(error?.localizedDescription ?? "unknown error")
+                        return
                     }
                     
-                case false:
-                    //ステータスコードの400範囲は障害、500範囲は内部システムエラー
-                    print("analysis failure: \(statusCode)")
+                    //レスポンスのステータスコードで条件分岐
+                    let statusCode = response?.statusCode
+                    switch statusCode == Optional(200) {
+                    case true:
+                        //取得対象をdocument_toneに設定し、JSON形式に整形
+                        let encoder = JSONEncoder()
+                        encoder.outputFormatting = .prettyPrinted
+                        guard let toneAnalysisJSON = try? encoder.encode(toneAnalyzerResult.documentTone) else {
+                            fatalError("Failed to encode to JSON.")
+                        }
+                        
+                        //正常に分析されたテキストをSwiftyJSONのJSON型へ変換
+                        let toneAnalysisValue = JSON(toneAnalysisJSON)
+                        
+                        //感情分析結果を配列に保存
+                        self.toneAnalysisArray.append(toneAnalysisValue)
+                        
+                        //全ての直列処理完了後に発火
+                        if self.toneAnalysisArray.count == self.arrayCount {
+                            print("All Process Done!")
+                            //jsonAnalysisOfToneAnalyzerの呼び出し
+                            self.jsonAnalysisOfToneAnalyzer()
+                        }
+                        
+                    case false:
+                        //ステータスコードの400範囲は障害、500範囲は内部システムエラー
+                        print("analysis failure: \(statusCode)")
+                    }
                 }
+                //直列処理完了
+                print("#\(i)End")
+                dispatchGroup.leave()
             }
         }
     }
@@ -142,9 +152,7 @@ class ToneAnalyzerModel {
                 joyCountArray.append(toneAnalysisArray[0].count+i-1)
             }
         }
-        print(joyCountArray.count)
-        print(joyCountArray.debugDescription)
         //最後にappendされた配列をControllerへ返す
-        //self.doneCatchAnalyzerProtocol?.catchAnalyzer(arrayAnalyzerData: joyCountArray)
+        self.doneCatchAnalyzerProtocol?.catchAnalyzer(arrayAnalyzerData: joyCountArray)
     }
 }
