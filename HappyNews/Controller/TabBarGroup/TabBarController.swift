644ff2a19373ee1f0dd4bd5fe19eaab1b27d47ce
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-12-16 06:57:46 +0900

    update edit-tableView2
    TabBarの不要なコードを削除

--- HappyNews/Controller/TabBarGroup/TabBarController.swift
+++ HappyNews/Controller/TabBarGroup/TabBarController.swift
@@ -7,13 +7,9 @@
 //
 
 import UIKit
-import Lottie
 
 class TabBarController: UITabBarController {
     
-    //インスタンスの作成
-    var loadingAnimationView = AnimationView()
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,55 +18,9 @@ class TabBarController: UITabBarController {
         
         //tabbarの色を変更
         tabBar.barTintColor = UIColor(hex: "ffffff")
-    
+        
         //tabbar背景の透過
         UITabBar.appearance().backgroundImage = UIImage()
-        
-        //アニメーションとラベルの呼び出し
-        addAnimationView()
-        addLabel()
-    }
-    
-    //アニメーションの準備
-    func addAnimationView() {
-        
-        //アニメーションファイルの指定
-        loadingAnimationView = AnimationView(name: "loading")
-        
-        //アニメーションの位置指定（画面中央）
-        loadingAnimationView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-        
-        //アニメーションのアスペクト比を指定＆ループで開始
-        loadingAnimationView.contentMode = .scaleAspectFit
-        loadingAnimationView.loopMode = .loop
-        loadingAnimationView.play()
-        
-        //アニメーションの背景色を設定し、Tab & Nav を隠す
-        loadingAnimationView.backgroundColor = UIColor.white
-        
-        //ViewControllerに配置
-        view.addSubview(loadingAnimationView)
-    }
-    
-    //loading中のラベルの準備
-    func addLabel() {
-        
-        //ラベルの生成
-        let loadingTextLabel = UILabel()
-        
-        //位置指定
-        loadingTextLabel.frame = CGRect(x: 0, y: 200, width: view.frame.size.width, height: view.frame.size.height)
-        
-        //テキストとカラーとフォントの設定
-        loadingTextLabel.text = "Happyなニュースを解析中..."
-        loadingTextLabel.textColor = UIColor.black
-        loadingTextLabel.font = UIFont.boldSystemFont(ofSize: 24)
-        
-        //横揃えの設定
-        loadingTextLabel.textAlignment = .center
-        
-        //ViewControllerに配置
-        view.addSubview(loadingTextLabel)
     }
     
     //tabbarをタップした場合のアクション
