fb622d992d5b218b6fe047240981fea196072674
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-12-25 21:34:38 +0900

    update master
    WebViewのNavigationbar設定をおこない、"閉じる"ボタンを設置してフルスクリーンを閉じる。

--- HappyNews/Controller/NewsGroup/WebViewController.swift
+++ HappyNews/Controller/NewsGroup/WebViewController.swift
@@ -14,14 +14,53 @@ class WebViewController: UIViewController,WKUIDelegate {
     //WKwebViewのインスタンス作成
     var webView = WKWebView()
     
+    //NavigatonBarのボタンのインスタンス
+    var closeButton: UIBarButtonItem?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        //webViewのサイズを決める
+        
+        //NavigationBarの呼び出し
+        webViewNavigationBar()
+        
+        //webViewのサイズ設定
         webView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
         
         //viewに反映
         view.addSubview(webView)
+        
+        //webViewの表示
+        openWebView()
+        
+        //スワイプで戻るを有効
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    //WebViewのNavigationBar設定
+    func webViewNavigationBar() {
+        
+        //NavigationBarの下からViewが始まる
+        self.navigationController?.navigationBar.isTranslucent = false
+        
+        //NavigationBarの色
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
+            
+        //NavigationBarのボタン設定
+        closeButton = UIBarButtonItem(title: "閉じる", style: .plain, target: self, action: #selector(tapCloseButton(_:)))
+        
+        //ボタン反映（右）
+        self.navigationItem.rightBarButtonItem = closeButton
+    }
+    
+    //closeボタンをタップしたときのアクション
+    @objc func tapCloseButton(_ sender: UIBarButtonItem) {
+        
+        //WebViewを閉じる
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func openWebView() {
         
         //TopNewsから受け取ったキー値urlで保存されている値をurlStringへ代入
         let urlString = UserDefaults.standard.object(forKey: "url")
