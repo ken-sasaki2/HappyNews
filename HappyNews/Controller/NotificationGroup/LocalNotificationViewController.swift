3e0a22c9ecc1060f7735a52861d94bdbebc86e47
ken-sasaki-222 <nkekisasa222@gmail.com>
2020-12-30 19:19:19 +0900

    create-LocalNotification
    ローカルPush通知を扱うControllerを作成。

---
+++ HappyNews/Controller/NotificationGroup/LocalNotificationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LocalNotificationViewController.swift
+//  HappyNews
+//
+//  Created by 佐々木　謙 on 2020/12/30.
+//  Copyright © 2020 佐々木　謙. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LocalNotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
