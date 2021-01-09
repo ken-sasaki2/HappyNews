6e15bc6f15b308f4bb7cc80f4f294deb38047f1d
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-01-09 10:50:39 +0900

    update create-TestCode
    テストコードのグループとサンプルクラスを作成し、動作確認を実装。

---
+++ HappyNewsTests/HappyNewsTests.swift
@@ -0,0 +1,33 @@
+//
+//  HappyNewsTests.swift
+//  HappyNewsTests
+//
+//  Created by 佐々木　謙 on 2021/01/08.
+//  Copyright © 2021 佐々木　謙. All rights reserved.
+//
+
+import XCTest
+
+class HappyNewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
