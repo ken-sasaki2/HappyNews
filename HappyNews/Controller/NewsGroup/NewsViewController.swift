47eb7fc09705cba52880d96b9b661691c12a5c99
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-01-14 11:38:40 +0900

    update ChangeNewsView
    XMLパースの実装。

--- HappyNews/Controller/NewsGroup/NewsViewController.swift
+++ HappyNews/Controller/NewsGroup/NewsViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 import SegementSlide
 
-class NewsViewController: UIViewController {
+class NewsViewController: UIViewController, XMLParserDelegate {
     
     //NewsTableViewのインスタンス
     @IBOutlet var newsTable: UITableView!
+    
+    //XMLファイルを保存するプロパティ
+    var xmlString: String?
+    
+    //XMLParserのインスタンスを作成
+    var parser = XMLParser()
+    
+    //RSSのパース内の現在の要素名を取得する変数
+    var currentElementName: String?
+
+    //NewsItemsモデルのインスタンス作成
+    var newsItems = [NewsItemsModel]()
+    
+    //RSSから取得するURLのパラメータを排除したURLを保存する値
+    var imageParameter: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +36,8 @@ class NewsViewController: UIViewController {
         setNewsNavigationBar()
         //scrollViewDidScroll(scrollView)
         
-        
+        //XML解析を開始する
+        settingXML()
     }
     
     //ニュースページのNavigationBar設定
@@ -38,9 +54,86 @@ class NewsViewController: UIViewController {
         self.navigationController?.navigationBar.isTranslucent = false
     }
     
+    // MARK: - XML Parser
+    //XMLファイルを特定してパースを開始する
+    func settingXML(){
+
+        //'社会'カテゴリのニュース（ニッポン放送）
+        xmlString = "https://news.yahoo.co.jp/rss/media/nshaberu/all.xml"
+
+        //XMLファイルをURL型のurlに変換
+        let url:URL = URL(string: xmlString!)!
+
+        //parserにurlを代入
+        parser = XMLParser(contentsOf: url)!
+
+        //XMLParserを委任
+        parser.delegate = self
+
+        //parseの開始
+        parser.parse()
+    }
     
+    //XML解析を開始する場合(parser.parse())に呼ばれるメソッド
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+
+        currentElementName = nil
+
+        if elementName == "item" {
+            newsItems.append(NewsItemsModel())
+        } else {
+            currentElementName = elementName
+        }
+    }
     
+    //"item"の中身を判定するメソッド(要素の解析開始と値取得）
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+
+        if newsItems.count > 0 {
+
+            //配列の番号を合わせる
+            //'link'と'image'はstringに分割で値が入るので初めて代入する値以外は取得しない
+            let lastItem = newsItems[newsItems.count - 1]
+            switch currentElementName {
+            case "title":
+                lastItem.title       = string
+                print(lastItem.title)
+            case "link":
+                if lastItem.url == nil {
+                    lastItem.url     = string
+                } else {
+                    break
+                }
+            case "pubDate":
+                lastItem.pubDate     = string
+            case "description":
+                lastItem.description = string
+            case "image":
+                //パラメータを排除して取得する
+                if lastItem.image == nil {
+                    imageParameter = string
+                    let imageURL = imageParameter!.components(separatedBy: "?")
+                    lastItem.image = imageURL[0]
+                } else {
+                    break
+                }
+            default:
+                break
+            }
+        }
+    }
     
+    //RSS内のXMLファイルの各値の</item>に呼ばれるメソッド（要素の解析終了）
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        //新しい箱を準備
+        self.currentElementName = nil
+    }
+
+    //XML解析でエラーが発生した場合に呼ばれるメソッド
+    func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
+        print("error:" + parseError.localizedDescription)
+    }
+
     //スクロールでナビゲーションバーを隠す
 //    func scrollViewDidScroll(_ scrollView: UIScrollView) {
 //        if scrollView.panGestureRecognizer.translation(in: scrollView).y < 0 {
