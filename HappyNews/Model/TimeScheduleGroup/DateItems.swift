1b399010348adcd71c0bc82cce52cfdfcd22488e
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-01-26 21:36:04 +0900

    update master
    前々回Pushの漏れ

---
+++ HappyNews/Model/TimeScheduleGroup/DateItems.swift
@@ -0,0 +1,16 @@
+//
+//  DateItems.swift
+//  HappyNews
+//
+//  Created by 佐々木　謙 on 2021/01/24.
+//  Copyright © 2021 佐々木　謙. All rights reserved.
+//
+
+import Foundation
+
+//現在時刻の取得や表示形式の変換で扱うクラス
+class DateItems {
+    static var date                = Date()
+    static var dateFormatter       = DateFormatter()
+    static var outputDateFormatter = DateFormatter()
+}
