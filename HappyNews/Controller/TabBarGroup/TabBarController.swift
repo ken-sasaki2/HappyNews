38ea6c71cbd21d2c53fc16a67298fc767080d104
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-01-29 18:13:36 +0900

    update master
    ダークモードになると非選択色の見え方が変わるので編集。

--- HappyNews/Controller/TabBarGroup/TabBarController.swift
+++ HappyNews/Controller/TabBarGroup/TabBarController.swift
@@ -18,6 +18,9 @@ class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // ダークモード適用を回避
+        self.overrideUserInterfaceStyle = .light
+        
         // 起動時のitemの色
         UITabBar.appearance().tintColor = UIColor(hex: "00AECC")
         
