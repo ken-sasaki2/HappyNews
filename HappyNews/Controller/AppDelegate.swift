b9dfe3821f9e138a000120b18838cf517499b2fd
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-01-29 17:47:31 +0900

    update master
    AppDelegateにて不要なコードを削除。

--- HappyNews/Controller/AppDelegate.swift
+++ HappyNews/Controller/AppDelegate.swift
@@ -14,16 +14,15 @@ import UserNotifications
 
 @available(iOS 14.0, *)
 @UIApplicationMain
-	class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
-
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    // 通知を許可するかどうかを促す画面を表示する設定
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        //Sign In With Appleに必要
         FirebaseApp.configure()
         
         if #available(iOS 10.0, *) {
-            // For iOS 10 display notification (sent via APNS)
             UNUserNotificationCenter.current().delegate = self
             
             let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
@@ -37,37 +36,10 @@ import UserNotifications
         }
         
         application.registerForRemoteNotifications()
-        // Override point for customization after application launch.
-        
         return true
     }
     
-    //プッシュ通知の受信処理
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-        // Print message ID.
-        if let messageID = userInfo["gcm.message_id"] {
-            print("Message ID: \(messageID)")
-        }
-        
-        // Print full message.
-        print(userInfo)
-    }
-    
-    //端末の電源がOFFの時やアプリが立ち上がっていない場合に呼ばれる（以下、バックグラウンド状態）
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        // Print message ID.
-        if let messageID = userInfo["gcm.message_id"] {
-            print("Message ID: \(messageID)")
-        }
-        
-        // Print full message.
-        print(userInfo)
-        
-        completionHandler(UIBackgroundFetchResult.newData)
-    }
-    
-    //アプリがバックグラウンド状態から戻ってきた場合に呼ばれる（以下、フォアグラウンド状態）
+    // アプリがバックグラウンド状態から戻ってきた場合に呼ばれる（以下、フォアグラウンド状態）
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
@@ -81,35 +53,18 @@ import UserNotifications
         
         completionHandler([ .badge, .sound, .banner ])
     }
-
-    //バックグラウンド状態できた通知通をタップ後、アプリに移動（起動）したら呼ばれる
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-        let userInfo = response.notification.request.content.userInfo
-        if let messageID = userInfo["gcm.message_id"] {
-            print("Message ID: \(messageID)")
-        }
-        
-        print(userInfo)
-        
-        completionHandler()
-    }
-
+    
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
