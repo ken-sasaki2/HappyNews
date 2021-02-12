b682a1c91aa5166cd3b62b6780949e49854e740e
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-02-12 19:10:15 +0900

    update create-TimeLine
    いいね機能で扱うクラスを作成。

---
+++ HappyNews/Model/TimeLineMessageGroup/LikeFunctionModel.swift
@@ -0,0 +1,16 @@
+//
+//  LikeFunctionModel.swift
+//  HappyNews
+//
+//  Created by 佐々木　謙 on 2021/02/12.
+//  Copyright © 2021 佐々木　謙. All rights reserved.
+//
+
+import Foundation
+
+// いいね機能で扱うクラス
+struct LikeFunction {
+    
+    let likeCount: Int
+    let likeFlagDictionaly: Dictionary<String, Any>
+}
