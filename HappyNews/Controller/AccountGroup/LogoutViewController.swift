d5977869a99c244f0c2be88576cdd627a5f64b9e
ken-sasaki-222 <nkekisasa222@gmail.com>
2021-02-22 19:56:46 +0900

    update master
    ログアウト後の画面に対してダークモードを回避するよう設定。

---
+++ HappyNews/Controller/AccountGroup/LogoutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogoutViewController.swift
+//  HappyNews
+//
+//  Created by 佐々木　謙 on 2021/02/22.
+//  Copyright © 2021 佐々木　謙. All rights reserved.
+//
+
+import UIKit
+
+class LogoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // ダークモード適用を回避
+        self.overrideUserInterfaceStyle = .light
+    }
+}
